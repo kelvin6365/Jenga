82cdd1ebd9bd60fea4bbc515b8ed06d4870f6012
Kelvin <kelvin6365@gmail.com>
2019-11-30 22:16:53 +0800

    set up ad mob
    added ADMOB setting key

---
+++ Assets/_DroppyTower/Scripts/CloudController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _DroppyTower
+{
+    public class CloudController : MonoBehaviour {
+
+        // Use this for initialization
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+            if (PlayerController.life <=0)
+            {
+                gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                float distance =gameObject.transform.position.y- GameManager.Instance.playerController.transform.position.y;
+                if (distance > 3 * PlayerController.height)
+                    Destroy(gameObject);
+            }
+    }
+    }
+}
